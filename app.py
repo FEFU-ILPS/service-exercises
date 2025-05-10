9eaf650c83d1afce78fda5f6abcece009b232982
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-05-10 19:53:00 +1000

    feature: Приложение FastAPI

---
+++ app.py
@@ -0,0 +1,22 @@
+from contextlib import asynccontextmanager
+
+from fastapi import FastAPI
+
+from database import disconnect_db
+from routers import health_router, exercises_router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # on_startup
+
+    yield
+
+    # on_shutdown
+    await disconnect_db()
+
+
+service = FastAPI(lifespan=lifespan)
+
+service.include_router(health_router)
+service.include_router(exercises_router)
